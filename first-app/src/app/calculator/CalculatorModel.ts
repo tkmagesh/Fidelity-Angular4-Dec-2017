48a1753e2338fe017ca05e8d9f68000ea0965bef
Magesh <tkmagesh77@gmail.com>
2017-12-27 16:35:39 +0530

    calculator one component

---
+++ first-app/src/app/calculator/CalculatorModel.ts
@@ -0,0 +1,23 @@
+//state & behavior of a calculator
+
+export class CalculatorModel{
+	n1 : number = 0;
+	n2 : number = 2;
+	result : number = 0;
+
+	add(){
+		this.result = this.n1 + this.n2;
+	}
+
+	subtract(){
+		this.result = this.n1 - this.n2;
+	}
+
+	multiply(){
+		this.result = this.n1 * this.n2;
+	}
+
+	divide(){
+		this.result = this.n1 / this.n2;
+	}
+}
